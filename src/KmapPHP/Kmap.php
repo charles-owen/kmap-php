64b3c7d963bf059de357d6c997d4ebf81a64924e
cbowen <cbowen@cse.msu.edu>
2021-01-17 17:29:48 -0500

    Working

--- src/KmapPHP/Kmap.php
+++ src/KmapPHP/Kmap.php
@@ -10,7 +10,14 @@ namespace CL\KmapPHP;
  * Server-side support for Kmap
  *
  * @cond
- * @property string appTag
+ * @property string appTag Application tag to send as a result of a test
+ * @property string name Filename to send as a result of a test
+ * @property int size Size of the map 2, 3, or 4
+ * @property bool test Set true if test results should be send to a server
+ * @property \CL\KmapPHP\KmapAPI api The API to set
+ * @property array minterms Array of minterms to use
+ * @property array dontcares Array of don't cares to use
+ * @property array labels Variable labels to use
  * @endcond
  */
 class Kmap {
@@ -23,12 +30,31 @@ class Kmap {
     }
 
     /**
-     * Reset the Cirsim object to no single, tests, or only options.
+     * Reset the Kmap object to simple size 3
      */
     public function reset() {
-        $this->appTag = null;
         $this->name = null;
-        $this->size = 3;        // Default size
+        $this->appTag = null;
+
+        $this->size = 3;
+        $this->manual = false;
+        $this->solve = false;
+        $this->solved = false;
+        $this->genDontCareOption = true;
+        $this->generator = true;
+        $this->fixed = false;
+        $this->verbose = true;
+        $this->resultSel = null;
+        $this->expressionSel = null;
+        $this->success = 'success';
+        $this->options = [];
+        $this->minterms = [];
+        $this->dontcare = [];
+        $this->genDontCare = false;
+        $this->labels = null;
+        $this->encode = false;
+
+        $this->test = false;
         $this->api = new KmapAPI();
     }
 
@@ -51,9 +77,6 @@ class Kmap {
             case 'api':
                 return $this->api;
 
-            case 'name':
-                return $this->name;
-
             default:
                 $trace = debug_backtrace();
                 trigger_error(
@@ -71,15 +94,6 @@ class Kmap {
      * <b>Properties</b>
      * Property | Type | Description
      * -------- | ---- | -----------
-     * answer | string | JSON answer to the problem
-     * appTag | string | If set, value is used as appTag for the file system
-     * components | array | Array of components to include
-     * export | boolean | If true, export/import menu options are available (default=true)
-     * save | boolean | If true, the save menu option is included (default=false)
-     * staff | boolean | If true, current user is a staff member (default=false);
-     * tab | string | Adds a single tab to the circuit
-     * tabs | array | Adds an array of tabs to the circuit
-     * tests | array | Array of Cirsim tests
      *
      * @param string $property Property name
      * @param mixed $value Value to set
@@ -111,8 +125,65 @@ class Kmap {
                 $this->name = $value;
                 break;
 
-            case 'size':
+            case 'encode':
+                $this->encode = $value;
+                break;
+
+            case 'test':
+                $this->test = $value;
+                break;
+
+            case "size":
                 $this->size = $value;
+                break;
+
+            case 'manual':
+                $this->manual = $value;
+                break;
+
+            case 'solve':
+                $this->solve = $value;
+                break;
+
+            case 'solved':
+                $this->solved = $value;
+                break;
+
+            case 'genDontCareOption':
+                $this->genDontCareOption = $value;
+                break;
+
+            case 'generator':
+                $this->generator = $value;
+                break;
+
+            case "minterms":
+                $this->minterms = $value;
+                break;
+
+            case 'map':
+                $this->map = $value;
+                break;
+
+            case "fixed":
+                $this->fixed = $value;
+                break;
+
+            case "verbose":
+                $this->verbose = $value;
+                break;
+
+            case "dontcare":
+            case "dontcares":
+                $this->dontcare = $value;
+                break;
+
+            case 'genDontCare':
+                $this->genDontCare = $value;
+                break;
+
+            case 'labels':
+                $this->labels = $value;
                 break;
 
             default:
@@ -129,17 +200,66 @@ class Kmap {
      * @return string HTML
      */
     public function present() {
-        $html = '';
+        $data = $this->options;
+        $data['size'] = $this->size;
 
-        $data = ['size'=>$this->size];
-
-        foreach($this->options as $option => $value) {
-            $data[$option] = $value;
+        if($this->fixed) {
+            $data['fixed'] = true;
         }
 
-        if($this->api->test !== null) {
+        if(!$this->map) {
+            $data['map'] = false;
+        }
+
+        if(!$this->generator) {
+            $data['generator'] = false;
+        }
+
+        if($this->manual) {
+            $data['manual'] = true;
+        }
+
+        if($this->solve) {
+            $data['solve'] = true;
+        }
+
+        if($this->solved) {
+            $data['solved'] = true;
+        }
+
+        if(!$this->verbose) {
+            $data['verbose'] = false;
+        }
+
+        if(!$this->genDontCareOption) {
+            $data['genDontCareOption'] = false;
+        }
+
+        if($this->genDontCare) {
+            $data['genDontCare'] = true;
+        }
+
+        if($this->resultSel !== null) {
+            $data['resultSel'] = $this->resultSel;
+            $data['expressionSel'] = $this->expressionSel;
+            $data['success'] = $this->success;
+        }
+
+        if(count($this->minterms) > 0) {
+            $data['minterms'] = $this->minterms;
+        }
+
+        if(count($this->dontcare) > 0) {
+            $data['dontcare'] = $this->dontcare;
+        }
+
+        if($this->labels !== null) {
+            $data['labels'] = $this->labels;
+        }
+
+        if($this->test && $this->api->test !== null) {
             // Api dependent features
-            $data['test'] = $this->api->test;
+            $data['testAPI'] = $this->api->test;
 
             if($this->name !== null) {
                 $data['name'] = $this->name;
@@ -150,10 +270,15 @@ class Kmap {
             }
         }
 
-        $payload = htmlspecialchars(json_encode($data), ENT_NOQUOTES);
-        $html .= '<div class="kmap-cl-install">' . $payload . '</div>';
 
-        return $html;
+        if($this->encode) {
+            $payload = base64_encode(htmlspecialchars(json_encode($data), ENT_NOQUOTES));
+
+        } else {
+            $payload = htmlspecialchars(json_encode($data), ENT_NOQUOTES);
+        }
+
+        return '<div class="kmap-cl-install">' . $payload . '</div>';
     }
 
     /**
@@ -165,8 +290,32 @@ class Kmap {
         $this->options[$option] = $value;
     }
 
-    private $size;
+    private $size;      // Size: 2, 3, or 4
     private $appTag;    // Application tag to send to test results
     private $name;      // Name to send to test results
     private $api;
+    private $options;               // Other options to set
+    private $test;      // Submit test results?
+
+    private $manual;                // Manual data entry?
+    private $solve;                 // Include Solve button?
+    private $solved;                // Present a solved map?
+    private $genDontCareOption;     // Generator has don't cares as an option?
+    private $generator;             // Display the generator
+    private $fixed;                 // Fixed minterm choice, no generator
+    private $genDontCare = false;	// Generate don't cares in problems
+    private $map = true;            // Include the actual map?
+
+    // A results selector. Selector that will be set to the success value
+    // if the expression successfully checks
+    private $resultSel;
+
+    private $success;               // Value resultSel will be set to if check is successful ('fail' otherwise)
+    private $expressionSel;         // Selector that will be set to the expression each time check is pressed.
+
+    private $verbose = true;		// Verbose response on mistakes
+    private $minterms = [];	        // The minterms for the problem
+    private $dontcare = []; 	    // Minterms we don't care about
+    private $labels = null;			// Optional array of labels to use
+    private $encode;                // base64 encode the div contents?
 }
